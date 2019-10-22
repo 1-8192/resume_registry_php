eee5199126af7320ee25466e87a817a9bce904e1
1-8192 <allegranzia@gmail.com>
2019-10-22 16:29:08 -0400

    view displays profile info properly

--- view.php
+++ view.php
@@ -2,9 +2,29 @@
     session_start();
     require_once "pdo.php";
 
-    //getting data from server
-    $stmt = $pdo->query("SELECT first_name, last_name, headline, profile_id FROM Profile ORDER BY last_name");
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+     //logging logic
+     if (!isset($_SESSION["name"])) {
+        die("ACCESS DENIED");
+    }
+
+     //Getting data for user to edit from db
+     $stmt = $pdo->prepare("SELECT * FROM Profile WHERE profile_id = :id");
+     $stmt->execute(array(":id" => $_GET['profile_id']));
+     $row = $stmt->fetch(PDO::FETCH_ASSOC);
+     
+     //making sure we're getting back an answer
+     if ($row === false) {
+         $_SESSION['error'] = "Bad value for profile id";
+         header("Location: index.php");
+         return;
+     } else {
+         $first_name = htmlentities($row['first_name']);
+         $last_name = htmlentities($row['last_name']);
+         $email = htmlentities($row['email']);
+         $headline = htmlentities($row['headline']);
+         $summary = htmlentities($row['summary']);
+         $profile_id = htmlentities($row['profile_id']);
+     }
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +34,14 @@
         <?php require_once 'bootstrap_styling.php' ?>
     </head>
     <body>
+     <div class="container">
         <h1>Profile information</h1>
+        <p>First Name: <?php echo $first_name ?></p>
+        <p>Last Name: <?php echo $last_name ?></p>
+        <p>Email: <?php echo $email ?></p>
+        <p>Headline: <?php echo $headline ?></p>
+        <p>Summary: <?php echo $summary ?></p>
+        <a href="index.php">Done</a>
+    </div>
     </body>
 </html>
