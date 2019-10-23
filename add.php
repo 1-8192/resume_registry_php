7f49b863f8d7b527e5b04ca1476a5b6227c8cef7
1-8192 <allegranzia@gmail.com>
2019-10-23 13:53:08 -0400

    checking position validity correctly

--- add.php
+++ add.php
@@ -24,23 +24,23 @@
 
     //Validate position fields
     function validatePos() {
-        for ($i=1; $i<=9; $i++) {
-            if (!isset($_POST['year'.$i])) continue;
-            if (!isset($POST['desc'.$i])) continue;
-
-            $year = $_POST['year'.$i];
-            $desc = $_POST['desc'.$i];
-
-            if (strlen($year) == 0 || strlen($desc) == 0 ) {
-                return 'All fields are required';
-            }
-
-            if (!is_numeric($year)) {
-                return "Position year must be numeric";
-            }
+        for($i=1; $i<=9; $i++) {
+          if ( ! isset($_POST['year'.$i]) ) continue;
+          if ( ! isset($_POST['desc'.$i]) ) continue;
+      
+          $year = $_POST['year'.$i];
+          $desc = $_POST['desc'.$i];
+      
+          if ( strlen($year) == 0 || strlen($desc) == 0 ) {
+            return "All fields are required";
+          }
+      
+          if ( ! is_numeric($year) ) {
+            return "Position year must be numeric";
+          }
         }
         return true;
-    }
+      }
 
     //PHP validation for input fields
     if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['headline']) && isset($_POST['summary'])) {
